0ae9548ac10be5d6dc3b0c61510a938ab9990804
Alexis <ajcodalify@gmail.com>
2024-03-17 17:16:22 +0800

    alexis changes

--- login.php
+++ login.php
@@ -8,8 +8,14 @@ include_once 'layout_head.php';
 
 <form action="">
     <div class="form-container">
+        <div class="header">
+            <span>Login</span>
+        </div>
         <div class="form-content">
-            <input type="text" name='name' placeholder="Email or Username" required>
+            <input type="text" name='email' placeholder="Username" required><br>
+            <input type="password" name='password' placeholder="Password" required><br>
+            <input type="submit" value='LOGIN'><br>
+            forgot password? <a href="">HERE!</a>
         </div>
     </div>
 </form>
