0cb784ec43f6bff579cda3d3de43b54c59f88455
Alexis <ajcodalify@gmail.com>
2024-03-30 18:29:30 +0800

    alexis changes

--- GTMS-sub.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$page_title = "GTMS-sub";
-include_once 'layout_head.php';
-
-
-include_once 'layout_foot.php';
-
-?>
--- try.php
+++
@@ -1,9 +0,0 @@
-<?php include_once "layout_head.php";
-
-
-
-
-
-
-include_once "layout_foot.php";
-?>
