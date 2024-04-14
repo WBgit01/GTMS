f3aa3ad258acec9c3f2321b6c8249d74a41fce7a
BravoWilldel <willdelbravo01@gmail.com>
2024-04-14 09:49:57 +0800

    04-14 update

--- navigation.php
+++ navigation.php
@@ -1,24 +1,23 @@
-<body>
 <header>
     <div class="navi">
-        <div class="logo"><a href="index.php">GTMS PORTAL</a></div>
+        <div class="logo">
+            <a href="index.php">
+                <img src="IMG/GTMS_Logo3.png" class="logo_nav">
+                GTMS PORTAL
+            </a>
+        </div>
         <ul class="links">
             <li><a href="about.php">About</a></li>
             <li><a href="services.php">Services</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
-        
         <div class="toggle_btn">
             <i class="fa-solid fa-bars-staggered"></i>
         </div>
     </div>
-
     <div class="drop-menu">
         <li><a href="about.php">About</a></li>
         <li><a href="services.php">Services</a></li>
         <li><a href="contact.php">Contact</a></li>
     </div>
 </header>
-<script src="script.js"></script>
-</body>
-
