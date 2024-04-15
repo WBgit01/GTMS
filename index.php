78f3baa850cdc42ab9e2a7643b956c52210481a2
BravoWilldel <willdelbravo01@gmail.com>
2024-04-15 21:12:13 +0800

    [200~

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once 'layout_head.php';
 
 <main>
 	<section id="content1">
-		<img src="IMG/MSC_Logo.png" class="logo2">
+		<img src="IMG/GTMS_Logo4.png" class="logo2">
 		<h1>Garments Transaction Management System</h1>
 		<p>Welcome to the GTMS Portal, your go-to platform for managing transactions seamlessly. <br>Designed with simplicity and efficiency in mind, Garments Transaction Management System or <br>GTMS Portal empowers students like you to effortlessly track and manage your transaction history with ease.</p>
 		<a href="login.php" class="action_btn form-open">Get Started</a>
