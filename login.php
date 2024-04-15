b81ed87a50830ae19d9c14322800a17d34e72a84
Alexis <ajcodalify@gmail.com>
2024-04-15 20:33:28 +0800

    AJ041124

--- login.php
+++ login.php
@@ -1,20 +1,55 @@
-<?php 
-include_once "config/database.php";
+<?php
 include_once "config/core.php";
 
 
-
 $page_title = "Login";
+$require_login = false;
+include_once "login_checker.php";
+$access_denied = false;
 include_once 'layout_head.php';
+
+if($_POST){
+    include_once "config/database.php";
+    include_once "object/user.php";
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $user = new User($db);
+
+    $user->email_address=$_POST['email_address'];
+    $user->student_id = $_POST['email_address'];
+
+    $email_exists_or_studentid_exist = $user->emailExists();
+    
+    //validate the login creds
+    
+    if ($email_exists_or_studentid_exist && password_verify($_POST['password'], $user->password)) {
+        $_SESSION['firstname'] = $user->firstname;
+        $_SESSION['lastname'] = $user->lastname;
+        $_SESSION['access_level'] = $user->access_level;
+
+        if ($user->access_level == "Admin") {
+            header("location:{$home_url}admin/index.php?action=login_success");
+        }else{
+            header("location:{$home_url}index.php?action=login_success");
+        }
+    }else{
+        $access_denied = true;
+    }
+}
+
+// error messages
+include_once 'alert-messages.php';
 ?>
 
 <i class="fa-solid fa-xmark form_close"></i>
     <!-- LOGIN FORM -->
 <div class="form login_form">
-    <form action="#">
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
         <h3>Login</h3>
             <div class="input_box">
-                <input type="text" placeholder="Enter Email or Student ID" required/>
+                <input type="text" placeholder="Enter Email or Student ID" name="email_address" required/>
                 <i class="fa-regular fa-envelope email"></i>
             </div>
 
