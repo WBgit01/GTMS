89c6f4be595f157e49a4dbf56703f2fc08a51863
Alexis <ajcodalify@gmail.com>
2024-04-15 21:49:15 +0800

    GTMS-71484

--- object/user.php
+++ object/user.php
@@ -18,38 +18,9 @@ class User {
 		$this->conn = $db;
 	}
 
-<<<<<<< HEAD
 	function createUser(){
 
 		
-=======
-	// Method to check if email exists in the database
-	public function emailExists() {
-		$query = "SELECT id FROM " . $this->table_name . " WHERE email_address = :email LIMIT 1";
-		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam(':email', $this->email_address);
-		$stmt->execute();
-		$rowCount = $stmt->rowCount();
-		return $rowCount > 0;
-	}
-
-	// Method to fetch user data by email address
-	public function getUserByEmail($email) {
-		$query = "SELECT id, firstname, lastname, access_level, password FROM " . $this->table_name . " WHERE email_address = :email LIMIT 1";
-		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam(':email', $email);
-		$stmt->execute();
-		return $stmt->fetch(PDO::FETCH_ASSOC);
-	}
-
-	// Method to generate IDs (Updated to be a static method)
-	public static function idGenerator() {
-		return '22' . chr(rand(97, 122)) . substr(md5(uniqid(mt_rand(), true)), 0, 5);
-	}
-
-	// Method to create a new user
-	public function createUser() {
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 		$this->created = date('Y-m-d H:i:s');
 		
 		// Sanitize data
@@ -75,7 +46,6 @@ class User {
 					created = :created"; 
 
 		$stmt = $this->conn->prepare($query);
-<<<<<<< HEAD
 
 		//sanitize
 		$this->firstname=htmlspecialchars(strip_tags($this->firstname));
@@ -93,8 +63,6 @@ class User {
 		$generated_student_id = $digits . 'b' . $randomNumber;
 
 
-=======
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 		$stmt->bindParam(':firstname', $this->firstname);
 		$stmt->bindParam(':lastname', $this->lastname);
 		$stmt->bindParam(':student_id', $generated_student_id);
@@ -124,7 +92,6 @@ class User {
 		print_r($stmt->errorInfo());
 		echo "</pre>";
 	}
-<<<<<<< HEAD
 
 	// check if given email exist in the database
 	function emailExists(){
@@ -165,7 +132,5 @@ class User {
 
 
 
-=======
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 }
 ?>
--- register.php
+++ register.php
@@ -3,10 +3,6 @@
 
 include_once "config/database.php";
 include_once "config/core.php";
-<<<<<<< HEAD
-=======
-include_once 'layout_head.php';
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 include_once "object/user.php";
 
 $database = new Database;
@@ -21,20 +17,11 @@ if ($_POST) {
     $user->lastname = $_POST['lastname'];
     $user->email_address = $_POST['email_address'];
     $user->password = $_POST['password'];
-<<<<<<< HEAD
     $user->access_level = "Student";
 
     if ($user->createUser()) {
         echo "<div class='alert-message-success'>";
             echo "Registration Success!";
-=======
-    $user->student_id = $_POST['student_id'];
-    $user->access_level = "Student"; // Set access level to "Student" by default
-
-    if ($user->createUser()) {
-        echo "<div class='alert-message-success'>";
-        echo "Registration Success! Redirecting to login page...";
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
         echo "</div>";
         // Redirect to login page after registration
         header("Location: login.php");
@@ -44,12 +31,8 @@ if ($_POST) {
             echo "Registration Failed, Please Try Again.";
         echo "</div>";
     }
-<<<<<<< HEAD
 } 
 
-=======
-}
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 ?>
 <i class="fa-solid fa-xmark form_close"></i>
 <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
@@ -60,7 +43,6 @@ if ($_POST) {
         <i class="fa-regular fa-user name"></i>
     </div>
 
-<<<<<<< HEAD
     <div class="input_box">
         <input type="name" name="lastname" placeholder="Last Name" required/>
             <i class="fa-regular fa-user name"></i>
@@ -84,55 +66,10 @@ if ($_POST) {
     </div>
             
     <button class="btn1" type="submit">Sign Up</button>
-=======
-<div class="form_center">
-    <div class="form_container">
-        <i class="fa-solid fa-xmark form_close"></i>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <h3>Signup</h3>
-            <div class="input_box">
-                <input type="text" name="firstname" placeholder="Enter your first name" required/>
-                <i class="fa-regular fa-user name"></i>
-            </div>
-
-            <div class="input_box">
-                <input type="text" name="lastname" placeholder="Enter your last name" required/>
-                <i class="fa-regular fa-user name"></i>
-            </div>
-
-            <div class="input_box">
-                <input type="text" name="student_id" placeholder="Enter your student ID" required/>
-                <i class="fa-regular fa-id-badge id name"></i>
-            </div>
-
-            <div class="input_box">
-                <input type="email" name="email_address" placeholder="Enter your email" required/>
-                <i class="fa-regular fa-envelope email"></i>
-            </div>
-
-            <div class="input_box">
-                <input type="password" name="password" placeholder="Create password" required/>
-                <i class="fa-solid fa-lock password"></i>
-                <i class="fa-regular fa-eye-slash pwhide"></i>
-            </div>
-
-            <div class="input_box">
-                <input type="password" name="confirm_password" placeholder="Confirm password" required/>
-                <i class="fa-solid fa-lock password"></i>
-                <i class="fa-regular fa-eye-slash pwhide"></i>
-            </div>
-            
-            <button type="submit" class="btn1">Signup Now</button>
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
 
     <div class="login_signup">
         Already have an account? <a href="login.php" id="login-link">Sign In Now!</a>
     </div>
 </form>
 
-<<<<<<< HEAD
 <?php include_once 'layout_foot.php'; ?>
-=======
-<?php include_once 'layout_foot.php' ?>
-
->>>>>>> 78f3baa850cdc42ab9e2a7643b956c52210481a2
