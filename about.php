918c54949a4632d07d431dcb391cd40b1d806e35
MayoresCristineJoy <mayores.cristinejoy@mscmarinduque.edu.ph>
2024-04-26 09:20:36 +0800

    GTMS-99414

--- about.php
+++ about.php
@@ -7,6 +7,11 @@ include_once 'layout_head.php';
 ?>
 
 <!-- about test 1-->
+ABOUT US
+Welcome to our Garment Transaction Management System (GTMS). Our platform is intended to completely change the way garment transactions are handled, increasing productivity, transparency, and ease of use.
+
+WHAT WE DO
+At GTMS, we provide an advanced platform that enables users to effectively handle their garment transactions. Our system handles every step of the process, including inventory management and order placement. Our goal is to minimize errors and delays in the garment transaction process by making it as smooth as possible.
 
 
 <?php include_once 'layout_foot.php'; ?>
