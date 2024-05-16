f2ac20e1b3d040ba3d96f88014df8a9a387680a5
BravoWilldel <willdelbravo01@gmail.com>
2024-05-16 10:43:00 +0800

    GTMS-61415

--- user/layout_head.php
+++ user/layout_head.php
@@ -6,13 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
-    <?php
-    if ($page_title =="Request details") {
-        echo "<link rel='stylesheet' href='../libs/css/invoice-style.css'>";
-    }else{
-        echo "<link rel='stylesheet' href='../libs/css/user-style.css'>";
-    }
-    ?>
+    <link rel="stylesheet" href="../libs/css/user-style.css">
     <title><?php echo $page_title; ?></title>
 </head>
 <body>
